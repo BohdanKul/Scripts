ad52eda7df8c04f39b5470e57e1b6205cb646e03
Bohdan.Kul <bohdan.kul@gmail.com>
2015-09-15 12:55:13 +0200

    Cleaned code and improved the interface

--- ed/gradascent.py
+++ ed/gradascent.py
@@ -21,12 +21,13 @@ def main():
 
     # setup the command line parser options 
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--data',       help='Data file',         type=str)
-    parser.add_argument('--inter','-I', help='Interactions file', type=str)
-
-    parser.add_argument('--beta', '-b', help='Inverse temperature ', type=float)
-    
-    parser.add_argument('-X', help='Lattice width ', type=int)
+    parser.add_argument('--data',       help='Data file',                  type=str)
+    parser.add_argument('--seed',       help='Seed used to generate data', type=int, default=0)
+    parser.add_argument('--time',       help='Benchmark execution time ',  action='store_true', default=False)
+    parser.add_argument('--verbose',    help='Verbose ',                   action='store_true', default=False)
+    parser.add_argument('--inter','-I', help='Interactions file',          type=str)
+    parser.add_argument('--beta', '-b', help='Inverse temperature ',       type=float)
+    parser.add_argument('-X',           help='Lattice width ',             type=int)
     parser.add_argument('-Y', help='Lattice height (set to 1 for a 1-d system)', type=int)
     parser.add_argument('--OBC', help='Set open boundary conditions ', action='store_true', default=False)
     parser.add_argument('-J', help='Ising interaction',  type=float)
@@ -70,11 +71,7 @@ def main():
         else:           bonds,  ZZ = PBC_Rect(int(args['X']), int(args['Y']), float(args['J']))
     Nbonds = len(ZZ)
    
-    #print X
-    #print Z
-    #print ZZ
     
-    t0 = time.time()
     print '---------Data acquisition----------'
     # Load or generate training set -------------------------------------------
     Nclamped = N 
@@ -99,13 +96,12 @@ def main():
             if i==0: probTable[i] = real(BM.evaluateProjector())
             else:    probTable[i] = probTable[i-1] + real(BM.evaluateProjector())
        
-        rm.seed(0)
+        rm.seed(args['seed'])
         data = []
         index = 1
         for i in range(Ndata):
             RN = rm.random()
             index = searchsorted(probTable, RN)
-            #index = 0
             cbits = bitfield(index)
             cbits = [0]*(Nclamped-len(cbits))+cbits.tolist() # keep the list length constant 
             data += [cbits]
@@ -113,9 +109,7 @@ def main():
 
     data = [[1, 0], [1,0], [1,0], [1,0], [0,1], [0,1], [0,1]]
     Ndata = 7
-    print 'Data: ', data
-    print 'Time: ', time.time() - t0
-    print 
+
     print '---------Gradient ascent----------'
     # Gradient descent --------------------------------------------------------
     
@@ -129,7 +123,6 @@ def main():
     # General an initial guess for the Hamiltonian
     gdZ  = np.random.randn(N)*max([max(Z),0.5])+Z
     gdX  = np.random.randn(N)*max([max(Z),0.5])+X
-    #gdX  = X
     gdZZ = np.random.randn(Nbonds)*max([max(Z),0.5])+ZZ
    
     gdZ  = np.array([-0.03040965, -0.04240908])
@@ -139,77 +132,116 @@ def main():
     print "Initial guess for X: ",  gdX
     print "Initial guess for ZZ: ", gdZZ
 
-    LL = .5/beta
-    RL = 0.05
-    nsteps = 100
+    # Initialize proportionality factor schedule
+    LL = .5/beta    # left limit
+    RL = 0.05       # right limit
+    nsteps = 5000    # number of steps
     b    = (1.0*nsteps*RL)/(LL - RL)
     a    = LL*b
     step = 0
-    Norm = 100.0
-    while ((step < nsteps) and (Norm > 0.01)):
-        print '----------------step ', step
-        # Initialize
+
+    Ts   = []
+    Norm = 100.0 # Norm of the gradient 
+    while ((step < nsteps) and (Norm > 0.001)):
+        
+        if args['verbose']: print '----------------step ', step, '-------------------' 
+        # Averages
         Zavers  = np.zeros((Ndata+1, N))
         Xavers  = np.zeros((Ndata+1, N))
         ZZavers = np.zeros((Ndata+1, Nbonds))
-        t0 = time.time()
-        BM = BoltzmannMachine(N, bonds, gdZ, gdX, gdZZ, beta)
-        #print ' Initialize BM time: ', time.time() - t0
         
+        
+        
+        if args['time']: t0 = time.time()
+        
+        BM = BoltzmannMachine(N, bonds, gdZ, gdX, gdZZ, beta)
+        
+        if args['time']: Ts += [time.time() - t0]
+        
+
+
         # Compute the log-likelihood
+        if args['time']: t0 = time.time()
+        
         LL = 0
-        t0 = time.time()
         for cbits in data:
             BM.setProjector(cbits)
             LL -= np.log(real(BM.evaluateProjector()))/(1.0 * Ndata)
-        print '   LL = %0.4f vs generating H LL = %0.4f' %(LL, gLL)
-        #print ' Compute LL time: ', time.time() - t0
+        if args['verbose']: print '--- LL = %0.4f vs %0.4f' %(LL, gLL)
+        else:               print '--- step=%03d LL = %0.4f vs %0.4f' %(step,LL, gLL)
         
+        if args['time']: Ts += [time.time() - t0]
+        
+
         # Accumulate averages
-        t0 = time.time()
+        if args['time']: t0 = time.time()
+        
         for i, cbits in enumerate(data):
             BM.setProjector(cbits)
             Zavers[i, :], Xavers[i,:], ZZavers[i,:] = BM.computeLocalAverages()
-            #print ' a: ', Zavers[i, :], Xavers[i,:], ZZavers[i,:]
             #Zavers[i, :], ZZavers[i, :] = -1.0*(np.array(cbits)*2-1)*beta, (cbits[0]*2-1)*(cbits[1]*2-1)*beta
-        #print ' Compute estimators time: ', time.time() - t0
         BM.setProjector([]) 
         Zavers[Ndata,:], Xavers[Ndata,:], ZZavers[Ndata,:] = BM.computeLocalAverages()
-        #print ' b: ', Zavers[Ndata,:], Xavers[Ndata,:], ZZavers[Ndata,:] 
-        #print Zavers[:Ndata]
-        #print Zavers[Ndata]
-        #print Zavers[:Ndata] - Zavers[Ndata]
+        
+        if args['time']: Ts += [time.time() - t0]
+        
+        
         # Compute derivatives and the norm
         dZ   = np.sum((Zavers[:Ndata]  - beta*Zavers[Ndata]),  axis=0)/(1.0*Ndata) 
-        #dX   = np.sum((Xavers[:Ndata]  - beta*Xavers[Ndata]),  axis=0)/(1.0*Ndata) 
-        dX   = np.zeros_like(Xavers[0])
+        dX   = np.sum((Xavers[:Ndata]  - beta*Xavers[Ndata]),  axis=0)/(1.0*Ndata) 
+        #dX   = np.zeros_like(Xavers[0])
         dZZ  = np.sum((ZZavers[:Ndata] - beta*ZZavers[Ndata]), axis=0)/(1.0*Ndata) 
-        Norm = np.sqrt(np.sum(dZ*dZ) + np.sum(dX*dX) + np.sum(dZZ*dZZ))
-        #print '   Norm = %0.3f' %Norm  
-        #print '  Unclamped <Z>:          ', Zavers[Ndata] 
-        #print '  Unclamped <X>:          ', Xavers[Ndata] 
-        #print '  Unclamped <ZZ>:         ', ZZavers[Ndata] 
-        #print '  Clamped <Z>:            ', Zavers[0], ' for ', data[0] 
-        #print '  Clamped <X>:            ', Xavers[0]
-        #print '  Clamped <ZZ>:           ', ZZavers[3],' for ', data[3]
-        #print '  Average clamped <Z>:    ', np.sum(Zavers[:Ndata] , axis = 0)/(1.0*Ndata)#*beta)
-        #print '  Average clamped <X>:    ', np.sum(Xavers[:Ndata] , axis = 0)/(1.0*Ndata)#*beta)
-        #print '  Average clamped <ZZ>:   ', np.sum(ZZavers[:Ndata], axis = 0)/(1.0*Ndata)#*beta)
         
+        
+        Norm = np.sqrt(np.sum(dZ*dZ) + np.sum(dX*dX) + np.sum(dZZ*dZZ))
         #eta   = a/(b+1.0*step)
         eta = 0.1
-        #print '  eta                     ', eta 
-        #print '  dZ:                     ', dZ
-        #print '  dX:                     ', dX
-        #print '  dZZ:                    ', dZZ
-
+       
         # Follow the negative gradient 
         gdZ   += -eta*dZ
         gdX   += -eta*dX
         gdZZ  += -eta*dZZ
-        #print '  Z field:                ', gdZ
-        #print '  X field:                ', gdX
-        #print '  ZZ field:               ', gdZZ
+       
+       
+        if  args['time']:
+            print '--- times:  BM=%0.3f LL=%0.3f E=%0.3f ' %(Ts[0], Ts[1], Ts[2])
+        if  args['verbose']:
+            print '--- data vs model averages: '
+
+            print '    Z:  ',
+            for i in range(N): 
+                zdata  = np.sum(Zavers[:Ndata,i] , axis = 0)/(1.0*Ndata)
+                zmodel = Zavers[Ndata,i]
+                print '(%+0.3f vs %+0.3f) ' %(zdata, zmodel),
+
+            print '\n    X:  ',
+            for i in range(N):
+                xdata  = np.sum(Xavers[:Ndata,i] , axis = 0)/(1.0*Ndata)
+                xmodel = Xavers[Ndata,i]
+                print '(%+0.3f vs %+0.3f) ' %(xdata, xmodel),
+
+            print '\n    ZZ: ',
+            for i in range(Nbonds): 
+                zzdata  = np.sum(ZZavers[:Ndata,i] , axis = 0)/(1.0*Ndata)
+                zzmodel = ZZavers[Ndata,i]
+                print '(%+0.3f vs %+0.3f) ' %(zzdata, zzmodel),
+            
+            print '\n--- algotirhm: '
+            print '    norm = %0.3f' %Norm  
+            print '    eta  = %0.3f' %eta  
+
+        
+            print '--- couplings: '
+            print '    Z:  ',
+            for i in range(N): print '%+0.3f ' %gdZ[i],
+            
+            print '\n    X:  ',
+            for i in range(N): print '%+0.3f ' %gdX[i],
+            
+            print '\n    ZZ: ',
+            for i in range(Nbonds): print '%+0.3f ' %gdZZ[i],
+            print
+            print
         # Clear the memory required to store ED solver
         del BM
         step += 1 
