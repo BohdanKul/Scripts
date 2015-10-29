71dba33ae72cae2b118fa7f5913f075a7461736f
Bohdan Kulchytskyy <bohdan.kul@gmail.com>
2015-10-29 14:06:12 -0700

    Tested on Evgeny's quantum training example. Added mixed support of sparse matrices. Unified output in one array. Improvded redability. Switched to matrix operations. Optimized execution: single call to calculate self.Z

--- library/bmachine.py
+++ library/bmachine.py
@@ -1,45 +1,59 @@
-import numpy               as np
-import scipy.sparse        as sparse
-import scipy.sparse.linalg as sl
+import numpy         as np
+import scipy.sparse  as sparse
+import scipy.linalg  as slin
 import Hbuilder
-
+import mexp                
 from   scipy.integrate import simps, trapz
 
 class BoltzmannMachine:
     def __init__(self, N, beta, **kwargs):
         self.Ns = N
         self.Nb = len(kwargs['Z2'])
-        I = sparse.eye(2)
-        X = sparse.csc_matrix(np.array([[0, 1], [1, 0]]))
-        Z = sparse.csc_matrix(np.array([[1, 0], [0, -1]]))
-        self.H = sparse.eye(2**N) - sparse.eye(2**N)
+        I = np.eye(2)
+        X = np.array([[0, 1], [1, 0]])
+        Z = np.array([[1, 0], [0, -1]])
+        self.H = np.zeros((2**N, 2**N))
+        #I = sparse.eye(2, format="csc")
+        #X = sparse.csc_matrix(np.array([[0, 1], [1, 0]]))
+        #Z = sparse.csc_matrix(np.array([[1, 0], [0, -1]]))
+        #self.H = sparse.csc_matrix((2**N, 2**N))
        
         for itype, bonds in kwargs.iteritems():
             if itype == 'X': oper = X 
             else:            oper = Z
             for bond in bonds:
-                #print itype, bond
-                #print Hbuilder.EmbedOper(oper, N, bond).todense()  
                 self.H = self.H + Hbuilder.EmbedOper(oper, N, bond)  
         
         self.beta = beta
-
-        self.rho  = sl.expm(-self.H*beta)
-
+        
+        #self.rho  = np.matrix(slin.expm(-self.H*beta), copy=False)
+        #self.H = np.matrix(self.H)
+        #self.rho  = mexp.expm(-self.H*beta)
+        
+        self.rho  = np.matrix(mexp.expm(-self.H*beta), copy=False)
+        
+        self.rhoN = self.rho/np.sum(self.rho.diagonal())
+        #print 'X1: ', kwargs['X'][:,-1]
+        #print 'Z1: ', kwargs['Z1'][:,-1]
+        #print 'Z2: ', kwargs['Z2'][:,-1]
+        #print "max H: ",   np.amax(np.abs(np.array([self.H.max(), self.H.min()]))),\
+        #      "max rho: ", np.amax(np.abs(np.array([self.rho.max(), self.rho.min()]))),\
+        #      "Z: ",       np.sum(self.rho.diagonal())
         self.operZs = []
         self.operXs = []
         for site in range(N):
-            self.operZs += [Hbuilder.EmbedOper(Z, N, [site, 1.0])]
-            self.operXs += [Hbuilder.EmbedOper(X, N, [site, 1.0])]
+            self.operZs += [Hbuilder.EmbedOper(Z, N, [site, 1.0], True)]
+            self.operXs += [Hbuilder.EmbedOper(X, N, [site, 1.0], True)]
             
         self.operZZs = []
         for bond in kwargs['Z2'].copy():
-            bond[2] = 1.0 
-            self.operZZs += [Hbuilder.EmbedOper(Z, N, bond)]
-        #print 'Hamiltonian:'
-        #print kwargs
-        #print self.H.todense()
-
+            bond[-1] = 1.0 
+            self.operZZs += [Hbuilder.EmbedOper(Z, N, bond, True)]
+        
+        #print
+        #print self.H
+        #print
+        #print self.rho
     def setProjector(self, cbits):
         self.clamped = False
         if  len(cbits)>0:
@@ -50,15 +64,18 @@ class BoltzmannMachine:
             self.P = sparse.kron(ket.transpose(), ket)
 
         if (len(cbits)>0) and (len(cbits)!=self.Ns): self.P = sparse.kron(self.P, sparse.qye(2**(self.N-len(cbits))))
-        elif (len(cbits)==0):                        self.P = sparse.eye(2**self.Ns)
+        elif (len(cbits)==0):                        self.P = sparse.eye(2**self.Ns, format="csc")
 
     def evaluateProjector(self):
-        return np.sum((self.rho*self.P).diagonal())/np.sum(self.rho.diagonal())
+        #print self.rhoN.diagonal()
+        #print self.rhoN*self.P.todense()
+        #print np.sum((self.rhoN*self.P).diagonal())
+        #print 
+        return np.sum((self.rhoN*self.P).diagonal())
     
     def computeLocalAverages(self, test = False):
         U  = self.rho*self.P
-        Z  = np.sum(U.diagonal())
-        U /= Z
+        U /= np.sum(U.diagonal())
         if  ((not self.clamped) or (test)):
             aves = np.zeros(self.Ns+self.Ns+self.Nb) 
             for site in range(self.Ns):
@@ -67,10 +84,10 @@ class BoltzmannMachine:
                                            )
                                     )
             
-                #aves[self.Ns+site] += np.real(np.sum(
-                #                                    (U*self.operXs[site]).diagonal()
-                #                                    )
-                #                             )
+                aves[self.Ns+site] += np.real(np.sum(
+                                                    (U*self.operXs[site]).diagonal()
+                                                    )
+                                             )
 
             for i in range(self.Nb):
                 aves[2*self.Ns+i] += np.real(np.sum(
@@ -79,14 +96,16 @@ class BoltzmannMachine:
                                             )
             return aves
         else:
-            Nsamples = 10 
+            Nsamples = 8 
             eZ  = np.zeros((Nsamples+1,self.Ns))
             eX  = np.zeros((Nsamples+1,self.Ns)) 
             eZZ = np.zeros((Nsamples+1,self.Nb))
             # Evaluate time-evolved operators on a discrete grid
-            tau = 1.0*self.beta/2.0/(Nsamples)
-            Ub0 = sl.expm( self.H*tau)
-            Uf0 = sl.expm(-self.H*tau)
+            tau = 1.0*self.beta/(2.0*Nsamples)
+            #Ub0 = np.matrix(slin.expm( self.H*tau), copy=False)
+            #Uf0 = np.matrix(slin.expm(-self.H*tau), copy=False)
+            Ub0 = np.matrix(mexp.expm( self.H*tau), copy=False)
+            Uf0 = np.matrix(mexp.expm(-self.H*tau), copy=False)
             for step in range(Nsamples+1):
                 if  step>0: U = Ub0*U*Uf0
 
@@ -95,7 +114,10 @@ class BoltzmannMachine:
                                                     (self.operZs[site]*U).diagonal()
                                                    )
                                             )
-                    #eX[step][site] = np.real((self.operXs[site]*U).tr())
+                    eX[step][site] = np.real(np.sum(
+                                                    (self.operXs[site]*U).diagonal()
+                                                    )
+                                            )
                 
                 for i in range(self.Nb):
                     eZZ[step][i] = np.real(np.sum(
@@ -105,10 +127,10 @@ class BoltzmannMachine:
         
             # Numerically integrate 
             UE = np.zeros(self.Ns+self.Ns+self.Nb) 
-            xs  = np.linspace(0.0, Nsamples+1-1, Nsamples+1)*tau
+            xs = np.linspace(0.0, Nsamples+1-1, Nsamples+1)*tau
             for site in range(self.Ns):
                 UE[site]            = trapz(eZ[:,site], xs)*2.0
-                #UE[self.Ns + site] += trapz(eX[:,site], xs)*2.0
+                UE[self.Ns + site] += trapz(eX[:,site], xs)*2.0
             
             for bond in range(self.Nb):
                 UE[2*self.Ns+bond] += [trapz(eZZ[:,bond], xs)*2.0]
