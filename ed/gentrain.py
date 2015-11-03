8f543915eb17e2d8c5b0e155a5325a08c32f43ba
Bohdan.Kul <bohdan.kul@gmail.com>
2015-11-03 12:23:55 -0500

    Renamed entropy flag.

--- ed/gentrain.py
+++ ed/gentrain.py
@@ -18,7 +18,7 @@ def main():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--mode', choices=['pair', 'triple', 'mean'], default='pair',    type=str)
     parser.add_argument('--alpha',     help='Amplitude of triple bond', default=0,    type=float)
-    parser.add_argument('--entropy',   help='Minimum desired dataset entropy', default=0,    type=float)
+    parser.add_argument('--minE',   help='Minimum desired dataset entropy', default=0,    type=float)
     parser.add_argument('--maxI',      help='Maximum number of iterations', default=1,    type=int)
     parser.add_argument('--maxD',      help='Maximum number of vectors in the dataset ', default=100,    type=int)
     parser.add_argument('--seed',      help='RN seed', default=0,    type=int)
@@ -40,7 +40,7 @@ def main():
     bsize    = 1000
     nI = 0
     weights = []
-    while (((not (Entropy > args['entropy'])) or (len(weights)>args['maxD'])) and (nI < args['maxI'])):
+    while (((not (Entropy > args['minE'])) or (len(weights)>args['maxD'])) and (nI < args['maxI'])):
         sites = np.arange(Ns)
         sites = sites.reshape((Ns,1))
         Ds    = np.zeros(Ns)
