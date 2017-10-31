e8849cd2b3538c3096692572d6dfcac1265f3b7c
BohdanKul <bohdan.kul@gmail.com>
2017-10-31 13:31:50 -0400

    Update

--- library/bmachine.py
+++ library/bmachine.py
@@ -2,7 +2,7 @@ import numpy         as np
 import scipy.sparse  as sparse
 import scipy.linalg  as slin
 import Hbuilder
-import mexp                
+import mexp
 from   scipy.integrate import simps, trapz
 
 def Bits2Int(bitlist):
@@ -24,25 +24,25 @@ class BoltzmannMachine:
         #X = sparse.csc_matrix(np.array([[0, 1], [1, 0]]))
         #Z = sparse.csc_matrix(np.array([[1, 0], [0, -1]]))
         #self.H = sparse.csc_matrix((2**N, 2**N))
-       
-        for itype, bonds in kwargs.iteritems():
-            if itype == 'X': oper = X 
+
+        for otype in ['X', 'Z1', 'Z2']:
+            if otype == 'X': oper = X
             else:            oper = Z
-            for bond in bonds:
-                self.H = self.H + Hbuilder.EmbedOper(oper, N, bond)  
-        
+            for bond in kwargs[otype]:
+                self.H = self.H + Hbuilder.EmbedOper(oper, N, bond)
+
         self.beta = beta
-        
+
         #self.rho  = np.matrix(slin.expm(-self.H*beta), copy=False)
         #self.H = np.matrix(self.H)
         #self.rho  = mexp.expm(-self.H*beta)
-        
+
         self.rho  = np.matrix(mexp.expm(-self.H*beta), copy=False)
-        
+
         #self.rhoN = self.rho/np.sum(self.rho.diagonal())
         self.rhoN = np.diagonal(self.rho).copy()
         self.rhoN /= np.sum(self.rhoN)
-       
+
         #print 'X1: ', kwargs['X'][:,-1]
         #print 'Z1: ', kwargs['Z1'][:,-1]
         #print 'Z2: ', kwargs['Z2'][:,-1]
@@ -58,19 +58,26 @@ class BoltzmannMachine:
                                 ).nonzero()[1]]
             #self.oper1Zs += [Hbuilder.EmbedOper(Z, N, [site, 1.0], True)]
             #self.operXs += [Hbuilder.EmbedOper(X, N, [site, 1.0], True)]
-            
+
         self.oper2Zs = []
         for bond in kwargs['Z2'].copy():
-            bond[-1] = 1.0 
+            bond[-1] = 1.0
             self.oper2Zs += [Hbuilder.EmbedOper(Z, N, bond, True).diagonal()]
             #self.oper2Zs += [Hbuilder.EmbedOper(Z, N, bond, True)]
 
         self.oper3Zs = []
         if 'tribody' in kwargs.keys():
-            for bond in kwargs['tribody'].copy():
-                bond[-1] = 1.0
+            for bond in list(kwargs['tribody']):
+                bond += [1.0]
                 self.oper3Zs += [Hbuilder.EmbedOper(Z, N, bond, True).diagonal()]
-        self.N3b = len(self.oper3Zs)    
+        self.N3b = len(self.oper3Zs)
+
+        self.oper4Zs = []
+        if 'quadbody' in kwargs.keys():
+            for bond in list(kwargs['quadbody']):
+                bond += [1.0]
+                self.oper4Zs += [Hbuilder.EmbedOper(Z, N, bond, True).diagonal()]
+        self.N4b = len(self.oper4Zs)
 
     def setProjector(self, cbits):
         self.bindex = Bits2Int(cbits)
@@ -88,41 +95,47 @@ class BoltzmannMachine:
     def evaluateProjector(self):
         #return np.sum((self.rhoN*self.P).diagonal())
         return self.rhoN[self.bindex]
-    
+
     def computeLocalAverages(self, test = False):
         #U  = self.rho*self.P
         #U /= np.sum(U.diagonal())
-        
+
         if  ((not self.clamped) or (test)):
             U = self.rho/np.sum(self.rho.diagonal())
-            aves = np.zeros(self.Ns+self.Ns+self.Nb) 
-            for site in range(self.Ns):
-                aves[site] = np.sum(self.oper1Zs[site]*U.diagonal().T)
+            aves = np.zeros(self.Ns + self.Ns + self.Nb + self.N3b + self.N4b)
+
+            # Z1 operator
+            for site in range(self.Ns): aves[site] = np.sum(self.oper1Zs[site]*U.diagonal().T)
                 #aves[site] = np.real(np.sum((U*self.oper1Zs[site]).diagonal()))
-            
-                aves[self.Ns+site] = 0 
-                for r,c in enumerate(self.operXs[site]):  
-                    aves[self.Ns+site] += U[c, r]
+
+            # X1 operator
+            shift = self.Ns
+            for site in range(self.Ns):
+                for r,c in enumerate(self.operXs[site]):
+                    aves[shift+site] += U[c, r]
                 #aves[self.Ns+site] += np.real(np.sum((U*self.operXs[site]).diagonal()))
 
-            for i in range(self.Nb):
-                aves[2*self.Ns+i] = np.sum(self.oper2Zs[i]*U.diagonal().T)
+            # Z2 operator
+            shift += self.Ns
+            for i in range(self.Nb): aves[shift+i] = np.sum(self.oper2Zs[i]*U.diagonal().T)
                 #aves[2*self.Ns+i] += np.real(np.sum((U*self.oper2Zs[i]).diagonal()))
 
-            for i in range(self.N3b):
-                aves[2*self.Ns+self.Nb] = np.sum(self.oper3Zs[i]*U.diagonal().T)
-            
+            # Z3 operator
+            shift += self.Nb
+            for i in range(self.N3b): aves[shift+i] = np.sum(self.oper3Zs[i]*U.diagonal().T)
+
+            # Z4 operator
+            shift +=self.N3b
+            for i in range(self.N4b): aves[shift+i] = np.sum(self.oper4Zs[i]*U.diagonal().T)
+
             return aves
         else:
             U  = np.matrix(np.zeros((2**self.Ns, 2**self.Ns)), copy=False)
-            Prob = self.rho[self.bindex, self.bindex] 
+            Prob = self.rho[self.bindex, self.bindex]
             U[:, self.bindex]  = self.rho[:,  self.bindex]/Prob
 
-            Nsamples = 12 
-            eZ   = np.zeros((Nsamples+1,self.Ns))
-            eX   = np.zeros((Nsamples+1,self.Ns)) 
-            e2Zs = np.zeros((Nsamples+1,self.Nb))
-            e3Zs = np.zeros((Nsamples+1,self.N3b))
+            Nsamples = 12
+	    eO   = np.zeros((Nsamples+1, self.Ns + self.Ns + self.Nb + self.N3b + self.N4b))
 
             # Evaluate time-evolved operators on a discrete grid
             tau = 1.0*self.beta/(2.0*Nsamples)
@@ -133,37 +146,42 @@ class BoltzmannMachine:
             for step in range(Nsamples+1):
                 if  step>0: U = Ub0*U*Uf0
 
+                shift = 0
                 for site in range(self.Ns):
-                    eZ[step][site] = np.sum(self.oper1Zs[site]*U.diagonal().T)
-                    #eZ[step][site] = np.real(np.sum((self.oper1Zs[site]*U).diagonal())) 
-                    
-                    eX[step][site] = 0
-                    for r,c in enumerate(self.operXs[site]):  
-                        eX[step][site] += U[c, r]
-                    #eX[step][site] = np.real(np.sum((self.operXs[site]*U).diagonal()))
-               
-                for i in range(self.Nb):
-                    e2Zs[step][i] = np.sum(self.oper2Zs[i]*U.diagonal().T)
-                    #e2Zs[step][i] = np.real(np.sum((self.oper2Zs[i]*U).diagonal()))
-                
-                for i in range(self.N3b):
-                    e3Zs[step][i] = np.sum(self.oper3Zs[i]*U.diagonal().T)
-        
-            # Numerically integrate 
-            UE = np.zeros(self.Ns+self.Ns+self.Nb+self.N3b) 
-            xs = np.linspace(0.0, Nsamples+1-1, Nsamples+1)*tau
-            offset = self.Ns
-            for site in range(self.Ns):
-                UE[site]            = trapz(eZ[:,site], xs)*2.0
-                UE[offset+site] += trapz(eX[:,site], xs)*2.0
-            
-            offset = 2*self.Ns
-            for bond in range(self.Nb):
-                UE[offset+bond] += [trapz(e2Zs[:,bond], xs)*2.0]
-            
-            offset = 2*self.Ns+self.Nb
-            for bond in range(self.N3b):
-                UE[offset+bond] += [trapz(e3Zs[:,bond], xs)*2.0]
+                    eO[step][site] = np.sum(self.oper1Zs[site]*U.diagonal().T)
+                    #eZ[step][site] = np.real(np.sum((self.oper1Zs[site]*U).diagonal()))
 
-            return UE 
-        
+                shift += self.Ns
+                for site in range(self.Ns):
+                    for r,c in enumerate(self.operXs[site]):
+                        eO[step][shift+site] += U[c, r]
+                    #eX[step][site] = np.real(np.sum((self.operXs[site]*U).diagonal()))
+
+                shift += self.Ns
+                for i in range(self.Nb):  eO[step][shift+i] = np.sum(self.oper2Zs[i]*U.diagonal().T)
+
+                shift += self.Nb
+                for i in range(self.N3b): eO[step][shift+i] = np.sum(self.oper3Zs[i]*U.diagonal().T)
+
+                shift += self.N3b
+                for i in range(self.N4b): eO[step][shift+i] = np.sum(self.oper4Zs[i]*U.diagonal().T)
+
+            # Numerically integrate
+            UE = np.zeros(self.Ns + self.Ns + self.Nb + self.N3b + self.N4b)
+            xs = np.linspace(0.0, Nsamples+1-1, Nsamples+1)*tau
+            shift = self.Ns
+            for site in range(self.Ns):
+                UE[site]       = trapz(eO[:,site], xs)*2.0
+                UE[shift+site] = trapz(eO[:,shift+site], xs)*2.0
+
+            shift += self.Ns
+            for bond in range(self.Nb):  UE[shift+bond] = trapz(eO[:,shift+bond], xs)*2.0
+
+            shift += self.Nb
+            for bond in range(self.N3b): UE[shift+bond] = trapz(eO[:,shift+bond], xs)*2.0
+
+            shift += self.N3b
+            for bond in range(self.N4b): UE[shift+bond] = trapz(eO[:,shift+bond], xs)*2.0
+
+            return UE
+
