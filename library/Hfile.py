ac7f888e4fc44611189c67fc317f4c753b7ddf25
BohdanKul <bohdan.kul@gmail.com>
2017-10-31 13:32:15 -0400

    Update

--- library/Hfile.py
+++ library/Hfile.py
@@ -5,34 +5,34 @@ from pylab    import real
 import collections
 
 
-#------------------------------------------------------------------------------ 
-#------------------------------------------------------------------------------ 
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 def EDtoMC(bits):
-    ''' 
-        Convert between ED and MC representation of Ising degrees of freedom 
+    '''
+        Convert between ED and MC representation of Ising degrees of freedom
         0 ->  1
         1 -> -1
     '''
     return (-np.array(bits)*2+1).tolist()
 
-#------------------------------------------------------------------------------ 
-#------------------------------------------------------------------------------ 
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 def MCtoED(bits):
-    ''' 
-        Convert between MC and ED representation of Ising degrees of freedom 
+    '''
+        Convert between MC and ED representation of Ising degrees of freedom
         1 ->  0
        -1 ->  1
     '''
     return ((-(np.array(bits)-1))//2).tolist()
 
-#------------------------------------------------------------------------------ 
-#------------------------------------------------------------------------------ 
-def bitfield(n):                                                                   
-    return np.array([1 if digit=='1' else 0 for digit in bin(n)[2:]]) 
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+def bitfield(n):
+    return np.array([1 if digit=='1' else 0 for digit in bin(n)[2:]])
 
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
-def LoadInters(fname):  
+def LoadInters(fname):
     header = open(fname, 'r').readlines()[1]
     (nSz, nSx, nSzSz) = header[1:].split()[0:3]
     (nSz, nSx, nSzSz) = (int(nSz), int(nSx), int(nSzSz))
@@ -50,40 +50,40 @@ def LoadInters(fname):
 #------------------------------------------------------------------------------
 # Acquire data
 #------------------------------------------------------------------------------
-def GetData(datafile, N, seed, Nsamples = 0, bonds = [], Z = [], X = [], ZZ = [], beta = 1.0): 
+def GetData(datafile, N, seed, Nsamples = 0, bonds = [], Z = [], X = [], ZZ = [], beta = 1.0):
     # Either from a pre-generated set
     if (datafile is not None):
         data = np.loadtxt(datafile, dtype="int")
         if data.ndim == 1: (Nsamples, Nclamped) = (1, len(data)); data = [data]
-        else:              (Nsamples, Nclamped) = data.shape 
-        
-        if Nclamped>N: 
+        else:              (Nsamples, Nclamped) = data.shape
+
+        if Nclamped>N:
             print 'Training set vectors exceed the graph size'
             return 0
-        
+
 
     # Or generate it from a  Hamiltonian at a given beta with help of ED
     else:
         Nclamped = N
-        if Nclamped>N: 
+        if Nclamped>N:
             print 'Training set vectors exceed the graph size'
             return 0
         BM = bm.BoltzmannMachine(N, bonds, Z, X, ZZ , beta)
         probTable = np.zeros(2**Nclamped)
         for i in range(2**Nclamped):
             cbits = bitfield(i)                              # convert i to a list of bits
-            cbits = [0]*(Nclamped-len(cbits))+cbits.tolist() # keep the list length constant   
+            cbits = [0]*(Nclamped-len(cbits))+cbits.tolist() # keep the list length constant
             BM.setProjector(cbits)
             if i==0: probTable[i] = real(BM.evaluateProjector())
             else:    probTable[i] = probTable[i-1] + real(BM.evaluateProjector())
-       
+
         rm.seed(seed)
         data = []
         for i in range(Nsamples):
             RN = rm.random()
             index = np.searchsorted(probTable, RN)
             cbits = bitfield(index)
-            cbits = [0]*(Nclamped-len(cbits))+cbits.tolist() # keep the list length constant 
+            cbits = [0]*(Nclamped-len(cbits))+cbits.tolist() # keep the list length constant
             data += [cbits]
         del BM
 
@@ -115,7 +115,8 @@ def getHeaders(filename):
 
 # ----------------------------------------------------------------------
 def cleanData(view):
-    if np.isnan(view[-1, 0]) or (view[-2, 0] < view[-1, 0]):
+
+    while np.isnan(view[-1, 0]) or (not(np.isnan(view[-2,0])) and (view[-2, 0] < view[-1, 0])):
         view = view[:-1,:]
 
     return view
